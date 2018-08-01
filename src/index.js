c08c8e5318d12d29d168a417514044183a642c94
lucasbertolo <lucasbertolo2@gmail.com>
2018-07-31 23:11:00 -0300

    adding project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger';
+import { searchRobots, requestRobots } from './reducers';
+import App from './containers/App'
+import registerServiceWorker from './registerServiceWorker';
+
+import 'tachyons';
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = 
+	createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+				<Provider store={store}>
+					<App />
+				</Provider> , document.getElementById('root'));
+registerServiceWorker();
+
