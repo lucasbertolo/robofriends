9fbd2d591989359edca1efe4eb594306a264f1c5
lucasbertolo <lucasbertolo@github.io>
2018-12-18 12:45:12 -0200

    Updates

---
+++ precache-manifest.ad0dc56659b7898919770ccd64b39de4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "b5cd64d592422e16d416",
+    "url": "/robofriends/static/js/main.b5cd64d5.chunk.js"
+  },
+  {
+    "revision": "3662b63b138e21de8a4d",
+    "url": "/robofriends/static/js/1.3662b63b.chunk.js"
+  },
+  {
+    "revision": "b5cd64d592422e16d416",
+    "url": "/robofriends/static/css/main.871d5332.chunk.css"
+  },
+  {
+    "revision": "3662b63b138e21de8a4d",
+    "url": "/robofriends/static/css/1.50c8b459.chunk.css"
+  },
+  {
+    "revision": "e0ab2fd915c740ae8891f4a24c28e27d",
+    "url": "/robofriends/index.html"
+  }
+];
